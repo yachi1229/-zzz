603c8160a7bbb7b50318a4788e55bd2e7ce134cb
yachi1229 <121242996+yachi1229@users.noreply.github.com>
2023-01-12 22:31:24 +0800

    MakeCode 專案的初始檔案

---
+++ images.g.ts
@@ -0,0 +1,26 @@
+// 自動產生的程式碼。請勿編輯。
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// 自動產生的程式碼。請勿編輯。
---
+++ tilemap.g.ts
@@ -0,0 +1,14 @@
+// 自動產生的程式碼。請勿編輯。
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// 自動產生的程式碼。請勿編輯。
